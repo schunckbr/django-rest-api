83493b3042869dbac85ec66660484f224ffff8c3
schunckbr <schunckbr@gmail.com>
2023-05-13 08:33:39 -0300

    projeto aula api rest do Django - Alura

---
+++ escola/views.py
@@ -0,0 +1,39 @@
+from rest_framework import viewsets, generics
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculaAlunoSerializer,ListaAlunosMatriculadosSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Listando todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """Listando todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    """Listando todas as matrículas"""
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+
+class ListaMatriculasAluno(generics.ListAPIView):
+    """Listando as matriculas do aluno"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculaAlunoSerializer
+
+class ListaAlunosMatriculados(generics.ListAPIView):
+    """Listano alunos(as) matriculados em um curso"""
+    def get_queryset(self):
+        queryset =Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class =ListaAlunosMatriculadosSerializer
